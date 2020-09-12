c0468c8937f54b39ac1b5bb420c6b93a18c2d6a2
Gideon <gideon@Mac-1s-MacBook-Pro.local>
2020-09-11 23:35:17 -0400

    solution for ex4 - positive and negative

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,16 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    result = []
+
+    # create a cache with array values as keys
+    num_dict = dict.fromkeys(a)
+
+    # loop through initial array
+    for i in a:
+        # check if value has a negative counterpart in the cache
+        if -i in num_dict and i > 0:
+            result.append(i)
 
     return result
 
